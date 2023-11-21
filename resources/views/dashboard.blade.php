6964b2452ca4e4c7f85f3b6f86a68e601d42d3ed
aryanando <aryanando.1505336@students.um.ac.id>
2023-11-22 05:47:16 +0700

    Fix Search Box

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -24,7 +24,7 @@
                                 </svg>
                             </div>
                             <input type="search" id="default-search"
-                                class="block w-full p-4 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-100 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                                class="block w-full p-4 ps-10 text-base text-gray-900 border border-gray-300 rounded-lg bg-gray-100 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                                 placeholder="Nama Barang" required>
                         </div>
                     </form>
